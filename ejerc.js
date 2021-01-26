05eade493866eb33ef38fa26469b10412ae0438e
Javier <javier.arancibia.reyes@hotmail.com>
2021-01-26 11:43:24 -0300

    Ready to push Javascript Promedio

---
+++ ejerc.js
@@ -0,0 +1,15 @@
+
+
+
+var num1 = parseInt(prompt("Ingresa un numero"))
+var num2 = parseInt(prompt("Ingresa otro numero"))
+
+
+
+
+function Promedio(num1, num2) {
+    var prom = (num1 + num2)/2
+    return prom
+}
+
+alert("El promedio de este algoritmo es " + Promedio(num1,num2))
